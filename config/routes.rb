435939108b5cd4ec157e338b5ac988a8c9b85097
sumit bansal <sumit.bansal@loconav.com>
2020-04-29 09:58:39 +0530

    Home page created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
 end
