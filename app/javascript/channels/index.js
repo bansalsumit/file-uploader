0a3de576083338694110094efcec05887b86d579
sumit bansal <sumit.bansal@loconav.com>
2020-05-13 14:45:43 +0530

    File size validations added

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,16 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+$( document ).ready(function() {
+  $('#attached-file').on('change', function() {
+    var numb = $(this)[0].files[0].size/1024/1024;
+    numb = numb.toFixed(2);
+    if(numb > 5){
+      $('#file-submit').prop('disabled', true);
+      alert('to big, maximum is 5MB. You file size is: ' + numb +' MB');
+    } else {
+      $('#file-submit').prop('disabled', false);
+    }
+  });
+});
--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -8,4 +8,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def size_range
+    1.byte..5.megabytes
+  end
 end
